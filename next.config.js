43f3eab9d942d8b7dd7a789fc050cf87126371a6
David Burrows <david@imergent.com>
2022-05-06 14:20:06 +0000

    Update 9 files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // providerImportSource: "@mdx-js/react",
+  },
+  reactStringMode: true,
+});
 
-module.exports = nextConfig
+module.exports = withMDX({
+  // Append the default value with md extensions
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,52 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { withCappedText } from "@ceteio/chakra-capsize/theme";
+import ralewayFontMetrics from "@capsizecss/metrics/raleway";
+import openSansFontMetrics from "@capsizecss/metrics/openSans";
+import literataFontMetrics from "@capsizecss/metrics/literata";
+import libreBaskervilleFontMetrics from "@capsizecss/metrics/libreBaskerville";
+import ptSerifFontMetrics from "@capsizecss/metrics/pTSerif";
+import "@fontsource/raleway/400.css";
+import "@fontsource/open-sans/700.css";
+import "@fontsource/literata/400.css";
+import "@fontsource/libre-baskerville/400.css";
+import "@fontsource/pt-serif/400.css";
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const fonts = {
+  heading: "Open Sans, sans-serif",
+  //body: "Raleway, sans-serif",
+  //body: "Literata, sans-serif",
+  //body: "Libre Baskerville, sans-serif",
+  body: "PT Serif, sans-serif",
+};
+
+const theme = extendTheme(
+  { colors, fonts },
+  withCappedText({
+    fontMetrics: {
+      Raleway: ralewayFontMetrics,
+      "Open Sans": openSansFontMetrics,
+      Literata: literataFontMetrics,
+      "Libre Baskerville": libreBaskervilleFontMetrics,
+      "PT Serif": ptSerifFontMetrics,
+    },
+  })
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
